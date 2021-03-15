b6aa16e02629190024991c23e4421103a95d57b2
jaeeeun93 <jaeeeun93@gmail.com>
2021-03-15 15:29:48 +0900

    타임리트 코드 추가

---
+++ src/main/java/kr/watw/jaeun/leafController.java
@@ -0,0 +1,15 @@
+package kr.watw.jaeun;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class leafController {
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "파라미터 입력") String name, Model model) {
+        model.addAttribute("name", name);
+        return "leafTest";
+    }
+}
--- src/main/java/kr/watw/jaeun/web/HelloController.java
+++ src/main/java/kr/watw/jaeun/web/HelloController.java
@@ -1,9 +1,8 @@
 package kr.watw.jaeun.web;
 
 import kr.watw.jaeun.web.dto.HelloResponseDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
