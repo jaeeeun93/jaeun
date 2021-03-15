ef3ed93c74204b5cda241088f00604a27f8f38b7
jaeeeun93 <jaeeeun93@gmail.com>
2021-03-15 18:26:22 +0900

    Authority Based Authorization 구현

--- src/main/java/kr/watw/jaeun/config/SecurityConfiguration.java
+++ src/main/java/kr/watw/jaeun/config/SecurityConfiguration.java
@@ -17,11 +17,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("adminPw")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("adminPw"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
                 .and()
-                .withUser("user").password(passwordEncoder().encode("userPw")).roles("USER")
+                .withUser("user")
+                .password(passwordEncoder().encode("userPw"))
+                .roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("managePw")).roles("MANAGER");
+                .withUser("manager")
+                .password(passwordEncoder().encode("managePw"))
+                .roles("MANAGER").authorities("ACCESS_TEST1");
     }
 
    @Override
@@ -33,6 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/manager/**").hasAnyRole("ADMIN","MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
    }
