5bdac6df74d6bba88ebb357979d4a976ce799eb4
jaeeeun93 <jaeeeun93@gmail.com>
2021-03-15 18:03:50 +0900

    스프링 시큐리티 HTTP Basic Authentication 추가

--- src/main/java/kr/watw/jaeun/leafController.java
+++ src/main/java/kr/watw/jaeun/leafController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class leafController {
-    @RequestMapping("/")
+    @RequestMapping("/leaf")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "파라미터 입력") String name, Model model) {
         model.addAttribute("name", name);
         return "leafTest";
