93007eb08f8054b59528abe7971bc2e1080133a1
jaeeeun93 <jaeeeun93@gmail.com>
2021-03-19 14:44:30 +0900

    thymeleaf layout 추가

--- src/main/java/kr/watw/jaeun/controller/HomeController.java
+++ src/main/java/kr/watw/jaeun/controller/HomeController.java
@@ -9,11 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping("index")
     public String index(){
+
+        System.out.println("ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
         return "index";
     }
 
     @GetMapping("login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("layoutTest")
+    public String layoutTest() {
+        return "layoutTest";
+    }
+
+    @GetMapping("layoutTest2")
+    public String layoutTest2() {
+        return "layoutTest2";
     }
 }
